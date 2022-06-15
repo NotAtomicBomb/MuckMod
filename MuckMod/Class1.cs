2d2b188898ab161d5b76646e4f6ba0b015bd5e5a
NotAtomicBomb <31144615+NotAtomicBomb@users.noreply.github.com>
2022-06-15 19:59:37 -0400

    Initial commit

---
+++ MuckMod/Class1.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace MuckMod
+{
+    [BepInPlugin("com.atomic.muck","Atomic's Muck Mod","0.0.1")]
+    public class Main : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> modEnabled { get; set; }
+        public void Awake()
+        {
+            registerConfig();
+            Logger.LogMessage("Config Registered");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+        }
+        private void registerConfig()
+        {
+            modEnabled = Config.Bind<bool>("Mod", "IsEnabled", true, "Do you want the mod to load or not.");
+        }
+
+
+
+        [HarmonyPatch(typeof(StructureSpawner),"FindObjectToSpawn")]
+        public static class Thing
+        {
+            private static void Postfix(StructureSpawner.WeightedSpawn[] structurePrefabs, ref GameObject __result)
+            {
+                if (__result.name == "TotemRespawn" && GameManager.gameSettings.multiplayer == GameSettings.Multiplayer.Off) 
+                {
+                    __result = structurePrefabs[0].prefab;
+                   
+                }
+            }
+
+        }
+    }
+
+    
+}
